e801125073977cbb1acfaa8095edfe361061dce8
Dmitriy Mosolov <dmitriy.mosolov@gmail.com>
2022-06-14 20:12:49 +0300

    Initial commit

---
+++ EShopCore.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿using EShopCore.DAL.Extensions;
+using Microsoft.Extensions.Hosting;
+
+var host = Host
+    .CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.UseDataAccessLayer();
+    })
+    .Build();
---
+++ EShopCore.DAL/DalDbContext.cs
@@ -0,0 +1,8 @@
+namespace EShopCore.DAL;
+
+using Microsoft.EntityFrameworkCore;
+
+public class DalDbContext: DbContext
+{
+    // TODO: Set context
+}
