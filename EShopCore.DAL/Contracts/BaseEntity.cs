34e56f452e731b37084afe24834ad6437115d94f
Dmitriy Mosolov <dmitriy.mosolov@gmail.com>
2022-06-15 14:50:07 +0300

    BaseEntity was fixed

--- EShopCore.DAL/Contracts/BaseEntity.cs
+++ EShopCore.DAL/Contracts/BaseEntity.cs
@@ -1,9 +1,11 @@
 namespace EShopCore.DAL.Contracts;
 
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 internal abstract class BaseEntity
 {
+    [Key]
     [Column("guid")]
     public Guid Guid { get; set; }
 
