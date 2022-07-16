d8c80684d774ea449aa22e651de63c1a50acbe6d
Dmitriy Mosolov <dmitriy.mosolov@gmail.com>
2022-07-16 15:55:35 +0300

    Updated

---
+++ EShop.Catalog.DAL/Migrations/20220716123741_CreateManufacturersTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EShop.Catalog.DAL.Migrations
+{
+    public partial class CreateManufacturersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "manufacturers",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    deleted_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_manufacturers", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "manufacturers");
+        }
+    }
+}
---
+++ EShop.Catalog.DAL/Migrations/20220716125405_AddColumnManufacturerIdToProductsTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EShop.Catalog.DAL.Migrations
+{
+    public partial class AddColumnManufacturerIdToProductsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_products_manufacturer_id",
+                table: "products",
+                column: "manufacturer_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_manufacturers_manufacturer_id",
+                table: "products",
+                column: "manufacturer_id",
+                principalTable: "manufacturers",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_manufacturers_manufacturer_id",
+                table: "products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_products_manufacturer_id",
+                table: "products");
+        }
+    }
+}
